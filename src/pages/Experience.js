7cef2debd64fe2488b669d8b51f870f19b00d17b
GuiltyGamingJay <jerontai07@gmail.com>
2023-02-18 05:26:37 -0500

    working ouit experience page

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,11 +1,95 @@
 import React from 'react';
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import SchoolIcon from "@material-ui/icons/School";
+import   "../styles/Experience.css";
+import WorkIcon from "@material-ui/icons/Work";
 
 function Experience() {
   return (
-    <div>
-      Experience
+    <div className="experience">
+      <VerticalTimeline lineColor="#3e497a">
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+      date="2008 -2012"
+      iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        
+      >
+         <h3 className="vertical-timeline-element-title">
+          Mcnair High School, Decatur, Georgia
+          </h3>
+          <p> High School Diploma </p>
+          </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2014 - 2018"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Atlanta Technical university, Atlanta , Georgia
+          </h3>
+
+          <h4 className="vertical-timeline-element-subtitle">
+            Associates Degree
+          </h4>
+
+          <p> Information Technology</p>
+        </VerticalTimelineElement> <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2022 -2023"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Georgia institute of Technology, Atlanta , Georgia
+          </h3>
+
+          <h4 className="vertical-timeline-element-subtitle">
+            Full-Stack-Web-Development Certificate
+          </h4>
+
+          <p> Certification</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2018 - 2021"
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+           CellPhone Repair Technician - Phone MAX
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            Atlanta, Georgia
+          </h4>
+          <p>	Completed multiple cell phone, Tablets, and electronic repairs </p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2021- present"
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            It Representative - comcast
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            Atlanta, Georgia
+          </h4>
+          <p>
+            Performs moves, adds, changes, and manages activities for desktop/laptop computers, telephones, and printers.
+          </p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
     </div>
   );
 }
+
 
 export default Experience;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,51 @@
 import React from 'react';
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import EmailIcon from "@material-ui/icons/Email";
+import GithubIcon from "@material-ui/icons/GitHub";
+import "../styles/Home.css";
 
 function Home() {
   return (
-    <div>
-      Home
-    </div>
+    <div className='home' >
+   <div className='about'> 
+  <h2>Hi, My Name is Je'Rontai Mcfee</h2>
+  <div className='prompt'> 
+   <p>
+   I am a very passionate student at the Georgia Institute of Technology. I have Developed and Honed my skills in their Full-stack Web Developer program.  </p>
+   
+   <LinkedInIcon />
+   <EmailIcon />
+   <GithubIcon />
+   </div>
+   </div>
+<div className='skills'>
+  <h1>Skills</h1>
+  <ol className='list'>
+    <li className='item'>
+      <h2>
+        Front-End
+      </h2>
+      <span>
+      ReactJS, Angular, Redux, HTML, CSS,   NPM,
+               BootStrap,  StyledComponents and willing to learn more.
+      </span>
+      </li>
+      <li className='item'>
+        <h2>Back-End</h2>
+        <span>
+        NodeJS, ExpressJS, GraphQL, ApolloServer,
+              MySQL, MongoDB, and willing to learn more.
+        </span>
+      </li>
+      <li className='item'>
+        <h2>Languages</h2>
+        Javascript, Python, ReactJS
+      </li>
+      </ol>
+      
+</div>
+</div> 
+    
   );
 }
 
