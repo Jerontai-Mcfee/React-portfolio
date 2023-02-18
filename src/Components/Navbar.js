446128e1891c3a9150ff7cb8b19dfcbb0d9924c3
GuiltyGamingJay <jerontai07@gmail.com>
2023-02-18 03:55:24 -0500

    Navbar page set, setLocation and icon used

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,14 +1,35 @@
-import React from 'react';
-import {Link} from "react-router-dom";
+import React, {useEffect, useState } from 'react';
+import {Link, useLocation} from "react-router-dom";
 import "../styles/Navbar.css";
+import ReorderIcon from "@material-ui/icons/Reorder";
 
 
 function Navbar() {
+    // Be sure to import useState into react.
+    const [expandNavbar, setExpandNavbar] = useState(false);
+
+// set it so it always knows where to set location when clicked
+    const location = useLocation();
+
+    useEffect(() => {
+        setExpandNavbar(false);
+    }, [location]);
+
   return (
-    <div className="navbar">
-        <div classname="toggleButton">
+
+    // if expandNavbar is true then you want to close if false
+    <div className="navbar" id={expandNavbar ? "open" : "close"}>
+        <div className="toggleButton">
             
-            <button></button>
+            <button 
+            onClick={() => {
+                setExpandNavbar((prev) => !prev);
+            }}
+            >
+                {/* Adds button for when it goes to different screen size */}
+                
+                <ReorderIcon />
+            </button>
         </div>
       <div className="Links">
       <Link to="/"> Home </Link>
